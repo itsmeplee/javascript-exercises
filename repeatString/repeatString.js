bb0b415a17ffe9164f0c9e0e5b49df4d783680be
Paul Lee <paul.plk@gmail.com>
2021-05-16 12:29:33 -0400

    complete helloWorld.js and repeatString.

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    let output = '';
+    for (let i = 0; i < count; i++) {
+        output += string;
+    }
+    return output;
 }
 
 module.exports = repeatString
