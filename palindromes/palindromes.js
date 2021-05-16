c1d1e758d5d793fce3d8d1b73ae3ac05f72fb59d
Paul Lee <paul.plk@gmail.com>
2021-05-16 13:21:25 -0400

    complete palindrome for single words. Does not work with multiple words

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,4 +1,10 @@
-const palindromes = function () {};
+const palindromes = function (str) {
+    let lowerCaseStr = str.toLowerCase();
+    console.log(lowerCaseStr)
+    let reverseStr = lowerCaseStr.split('').reverse().join('');
+    console.log(reverseStr)
+    return lowerCaseStr === reverseStr;
+};
 
 
 module.exports = palindromes
