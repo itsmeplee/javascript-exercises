0fd1ca8217edfa261394e0e23b529b1e1ce871be
Paul Lee <paul.plk@gmail.com>
2021-05-17 13:28:09 -0400

    complete fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,3 +1,9 @@
-const fibonacci = function () {};
+const fibonacci = function (num) {
+    let fibNums = [0, 1];
+    for (let i = 2; i <= num; i++) {
+        fibNums.push(fibNums[i - 1] + fibNums[i - 2]);
+    }
+    return fibNums[num];
+};
 
 module.exports = fibonacci
