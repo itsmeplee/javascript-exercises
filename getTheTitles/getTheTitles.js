df3340c9f6138fb8aa85d62a4de66a98701824aa
Paul Lee <paul.plk@gmail.com>
2021-05-17 13:31:23 -0400

    complete getTheTitle.js

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrOfObjs) {
+    let titles = [];
+    for (let i = 0; i < arrOfObjs.length; i++) {
+        titles.push(arrOfObjs[i]['title']);
+    }
+    return titles;
 }
 
 module.exports = getTheTitles;
