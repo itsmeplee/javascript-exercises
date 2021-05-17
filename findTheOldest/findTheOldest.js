a9aa2b03cb6dcceeae96c87059724bab451c299f
Paul Lee <paul.plk@gmail.com>
2021-05-17 14:22:01 -0400

    complete findTheOldest.js

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,3 +1,32 @@
-let findTheOldest = function() {
-}
+let findTheOldest = function (people) {
+  let oldest = "";
+  let oldestAge = 0;
+  console.log(people.length);
+  for (let i = 0; i < people.length; i++) {
+    let age = people[i]["yearOfDeath"] - people[i]["yearOfBirth"];
+    if (oldestAge < age) {
+      oldestAge = age;
+      oldest = people[i]["name"];
+    }
+  }
+  return oldest;
+};
+
+// let findTheOldest = function (people) {
+//   let oldest;
+//   let oldestAge = 0;
+//   for (let i = 0; i < people.length; i++) {
+//     if ("yearOfDeath" in people[i]) {
+//       let age = people[i]["yearOfDeath"] - people[i]["yearOfBirth"];
+//       if (oldestAge < age) {
+//         oldestAge = age;
+//         oldest = people[i]["name"];
+//       }
+//     } else {
+//       oldest = people[i]["name"];
+//     }
+//   }
+//   return oldest;
+// };
+
 module.exports = findTheOldest;
